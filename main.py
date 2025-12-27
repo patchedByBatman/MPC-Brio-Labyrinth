bb7030656d4d71a33291fd6863798d3bff8b5391
patchedByBatman <gunturicm@gmail.com>
2025-12-27 18:24:56 +0000

    integrated KF and now the simulation is using non-linear dynamics. Completed work. need to refactor and clean up the code.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import numpy as np
 from matplotlib.patches import Circle
 from matplotlib.animation import FuncAnimation, FFMpegWriter
 from polytope import extreme
+from scipy.integrate import odeint
 
 mng = og.tcp.OptimizerTcpManager("optimizer/brio_labyrinth")
 # Start the TCP server
@@ -56,7 +57,7 @@ def get_pos_bounds(poly):
     return xmin, xmax, ymin, ymax
 
 Ts = 1E-1
-sim_time = 90
+sim_time = 100
 tsim = np.linspace(0, sim_time, int(sim_time/Ts))
 nz = 6
 nu = 2
@@ -87,14 +88,18 @@ z_0 = np.array([path[0][0], 0, path[0][1], 0, 0, 0])
 z_current = z_0
 state_sequence = np.zeros((tsim.shape[0] + 1, nz))
 input_sequence = np.zeros((tsim.shape[0] + 1, nu))
+# states_measured = np.random.multivariate_normal(mean=[0.]*nz, cov=np.diagflat([0.00001, 0.00001, 0.00001, 0.00001, np.pi/240000, np.pi/240000]), size=(tsim.shape[0] + 1, )).reshape(tsim.shape[0] + 1, nz)
+states_measured = np.random.multivariate_normal(mean=[0.]*nz, cov=np.diagflat([0.0001, 0.0001, 0.0001, 0.0001, np.pi/5000, np.pi/5000]), size=(tsim.shape[0] + 1, )).reshape(tsim.shape[0] + 1, nz)
+states_measured[0, :] += np.copy(z_current.reshape((6, )))
 
 for i, t in enumerate(tsim):
-    point = (z_current[0], z_current[2])
+    kf_est = kf.update(np.array([states_measured[i][4], states_measured[i][5]]).reshape([nu, 1]), np.array([states_measured[i][0], states_measured[i][2]]).reshape([2, 1]))
+    point = (kf_est[0][0], kf_est[2][0])
 
     rt.update_rays(point)
     intersections = rt.compute_ray_intersections(lb.walls + lb.holes)
 
-    poly = csc.get_convex_set(rt.starting_point, intersections, path, 0)
+    poly = csc.get_convex_set(rt.starting_point, intersections, path, get_projection_on_path(path, point)[0])
     xmin, xmax, ymin, ymax = get_pos_bounds(poly)
     poly_patch = get_patch(poly, color='lightblue')
     pos_ref = get_pos_ref(path, poly, point, 20)
@@ -103,9 +108,10 @@ for i, t in enumerate(tsim):
         pos_ref = point
     z_ref = np.array([pos_ref[0], 0, pos_ref[1], 0, 0, 0]).reshape((6, 1))
 
+    states_with_kf_est = [kf_est[0][0], states_measured[i][1], kf_est[2][0], states_measured[i][3], states_measured[i][4], states_measured[i][5]]
     # pass NMPC problem parameters
     solver_response = mng.call(p=[
-            *z_current.flatten(), *z_ref.flatten(),
+            *states_with_kf_est, *z_ref.flatten(),
             xmin, xmax, ymin, ymax
         ])
     assert solver_response.is_ok(), "solver failed!"
@@ -117,20 +123,23 @@ for i, t in enumerate(tsim):
 
     # use the first control action from the computed sequence of solutions and simulate one time step
     u_mpc = us[0:nu]
-    z_next = np.array(dyn.linear_dynamics_dt(z_current, u_mpc))
+    states, info = odeint(func=dyn.non_linear_dynamics_ct, y0=z_current.reshape((6, )), t=np.linspace(0, Ts, 10), full_output=True, args=(lambda t: u_mpc[0], lambda t: u_mpc[1]))
+    z_next = np.array(states[-1]).reshape(nz, 1)
     # cache results
     intersections_list.append(intersections)
     poly_patches.append(poly_patch)
-    ball_patches.append(Circle(point, 0.5, fc="red"))
+    ball_patches.append(Circle((states[-1][0], states[-1][2]), 0.5, fc="red"))
     pos_refs.append(pos_ref)
     state_sequence[i+1, :] = z_next.T
     input_sequence[i, :] = u_mpc
+    states_measured[i+1, :] += z_next.reshape((6, ))
     z_current = z_next.flatten()
 
 
 # kill the server once simulation in complete
 mng.kill()
 print(state_sequence)
+kf_estimates = np.array(kf.measurement_update_cache()[0]).reshape([-1, 5]).T
 # for current_idx_on_path, point in enumerate(path):
 #     ball_patches.append(Circle(point, 0.5, fc="red"))
 #     rt.update_rays(point)
@@ -139,7 +148,7 @@ print(state_sequence)
 #     intersections_list.append(intersections)
 #     poly_patch = get_patch(poly, color='lightblue')
 #     poly_patches.append(poly_patch)
-an.pos_ax.scatter([point[0] for point in path], [point[1] for point in path])
+an.pos_ax.scatter([point[0] for point in path], [point[1] for point in path], s=1)
 # scatters[0] = an.pos_ax.scatter([intersection[1][0] for intersection in intersections], [intersection[1][1] for intersection in intersections])
 def update_animation(frame):
     # scatters[0].remove()
@@ -151,7 +160,49 @@ def update_animation(frame):
     poly_patch = an.pos_ax.add_patch(poly_patch)
     ball_patch = an.pos_ax.add_patch(ball_patch)
     an.update_pos_ref(pos_refs[frame])
-    return poly_patch, ball_patch, an.pos_ref
+
+    t = tsim[0:frame]
+    vx = state_sequence[0:frame, 1]
+    vy = state_sequence[0:frame, 3]
+    vx_mes = states_measured[0:frame, 1]
+    vy_mes = states_measured[0:frame, 3]
+    vx_est = kf_estimates[1, 0:frame]
+    vy_est = kf_estimates[3, 0:frame]
+    an.vx_mes_line.set_data(t, vx_mes)
+    an.vy_mes_line.set_data(t, vy_mes)
+    an.vx_est_line.set_data(t, vx_est)
+    an.vy_est_line.set_data(t, vy_est)
+    an.vx_line.set_data(t, vx)
+    an.vy_line.set_data(t, vy)
+
+    a = state_sequence[0:frame, 4]
+    b = state_sequence[0:frame, 5]
+    a_mes = states_measured[0:frame, 4]
+    b_mes = states_measured[0:frame, 5]
+    an.alpha_mes_line.set_data(t, a_mes)
+    an.beta_mes_line.set_data(t, b_mes)
+    an.alpha_line.set_data(t, a)
+    an.beta_line.set_data(t, b)
+
+    x = state_sequence[0:frame, 0]
+    y = state_sequence[0:frame, 2]
+    x_mes = states_measured[0:frame, 0]
+    y_mes = states_measured[0:frame, 2]
+    x_est = kf_estimates[0, 0:frame]
+    y_est = kf_estimates[2, 0:frame]
+    an.x_mes_line.set_data(t, x_mes)
+    an.y_mes_line.set_data(t, y_mes)
+    an.x_est_line.set_data(t, x_est)
+    an.y_est_line.set_data(t, y_est)
+    an.x_line.set_data(t, x)
+    an.y_line.set_data(t, y)
+
+    an.g_line.set_data(t, kf_estimates[4, 0:frame])
+
+    return poly_patch, ball_patch, an.pos_ref, an.vx_line, an.vy_line, an.vx_mes_line, \
+                an.vy_mes_line, an.vx_est_line, an.vy_est_line, an.alpha_line, an.beta_line, \
+                an.alpha_mes_line, an.beta_mes_line, an.x_line, an.y_line, an.x_mes_line, \
+                an.y_mes_line, an.x_est_line, an.y_est_line, an.g_line
     
 animation = FuncAnimation(fig=an.fig, func=update_animation, frames=range(len(ball_patches)), interval=100)
 an.build_labyrinth()
@@ -170,8 +221,8 @@ writer = FFMpegWriter(
 
 # Save animation
 try:
-    animation.save('manual_ball_positioning_feasible_convex_sets2.mp4', writer=writer)
-    print("Animation saved as gpu_animation.mp4 using GPU acceleration.")
+    animation.save('nonlin_sim.mp4', writer=writer)
+    print("Animation saved as nonlin_sim.mp4.")
 except Exception as e:
     print(f"Error saving animation: {e}")
 # animation.save('manual_ball_positioning_feasible_convex_sets2.mp4', writer='ffmpeg', fps=30, bitrate=1800, dpi=400)
